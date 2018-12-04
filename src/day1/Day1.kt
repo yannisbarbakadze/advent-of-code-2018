65c0d7e7bf913031da1c32a29c9783568ff357c9
yannisbarbakadze <johnbarbakadze@gmail.com>
2018-12-04 23:40:39 +0000

    Added files

---
+++ src/day1/Day1.kt
@@ -0,0 +1,42 @@
+package day1
+
+import java.io.File
+
+private val input = readFile("input.txt").map { it.toInt() }
+
+fun main(args: Array<String>) {
+    println(part1())
+    println(part2())
+}
+
+/**
+ * Map list to integers and return their sum
+ */
+fun part1(): Int = input.sum()
+
+/**
+ * Returns a new BufferedReader for reading the contents of the file.
+ * Once we have a BufferedReader, we can read all the lines in it
+ */
+fun readFile(fileName: String): List<String> = File("src/day1/$fileName").bufferedReader().readLines()
+
+
+/**
+ * Infinitely loops through input list and adds currentFrequency in
+ * a set until it finds a frequency that is reached before
+ */
+fun part2(): Int {
+    val frequenciesReachedOnce = mutableSetOf<Int>()
+    var currentFrequency = 0
+
+    while (true) {
+        for (i in input) {
+            if (currentFrequency !in frequenciesReachedOnce) {
+                frequenciesReachedOnce.add(currentFrequency)
+                currentFrequency += i
+            } else {
+                return currentFrequency
+            }
+        }
+    }
+}
